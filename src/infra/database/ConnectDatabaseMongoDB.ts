4bf74a5b06c505c9f0e0e55c9ddc2b3c815eecb3
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2024-01-30 10:14:43 -0300

    Refactoring ConnectDatabaseMongoDB

--- src/infra/database/ConnectDatabaseMongoDB.ts
+++ src/infra/database/ConnectDatabaseMongoDB.ts
@@ -3,8 +3,14 @@ import { PrismaClient } from '../../../prisma/generated';
 export class ConnectDatabaseMongoDB {
 	private static connection: PrismaClient;
 
-	constructor() {
-		ConnectDatabaseMongoDB.connection = new PrismaClient();
+	constructor(url_connection?:string) {
+		let datasources = {};
+		if (url_connection) {
+			datasources = { db: { url: url_connection } };
+		}
+		if (!ConnectDatabaseMongoDB.connection) {
+			ConnectDatabaseMongoDB.connection = new PrismaClient(datasources);
+		}
 	}
 
 	getInstance(): PrismaClient {
